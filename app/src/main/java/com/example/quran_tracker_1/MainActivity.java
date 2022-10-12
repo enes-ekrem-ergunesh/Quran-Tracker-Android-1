d196873cbe40154c00e85c201b952c3d45ced79f
enesekremergunesh <enes.ekrem.ergunes@stu.ibu.edu.ba>
2022-10-12 08:36:02 +0200

    Custom Action Bar added.

--- app/src/main/java/com/example/quran_tracker_1/MainActivity.java
+++ app/src/main/java/com/example/quran_tracker_1/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.quran_tracker_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* Layout variables */
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Layout variables */
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+
+        toolbar.getMenu().getItem(1).setOnMenuItemClickListener(menuItem -> {
+            Toast.makeText(getApplicationContext(), "Added :)", Toast.LENGTH_LONG).show();
+            return false;
+        });
 
     }
 }
